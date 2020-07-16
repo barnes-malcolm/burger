6132a4238b2dd766661fc9713078b1f78da84649
Malcolm Barnes <barnes.malcolm@gmail.com>
2020-07-15 22:12:54 -0500

    Server initialized. Next step, to create SQL database.

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+//edit this
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//edit this
+var routes = require("./controllers/catsController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+
+  console.log("Server listening on: http://localhost:" + PORT);
+});
