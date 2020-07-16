62f91f6a67eae616fb9d02801d89878e032f4e17
Malcolm Barnes <barnes.malcolm@gmail.com>
2020-07-15 22:21:31 -0500

    Created and added data to seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Lil Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lil Burger', true);
