2be54d7877808b4c7abc75366afa0b02ce048f49
Malcolm Barnes <barnes.malcolm@gmail.com>
2020-07-16 20:38:09 -0500

    Created ORM and burger model. Next step, controller setup

---
+++ config/orm.js
@@ -0,0 +1,43 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+var orm = {
+    selectAll: function(table, cb) {
+        var dbQuery = "SELECT * FROM" + table + ";";
+
+        connection.query(dbQuery, function(err, res){
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    }
+    insertOne: function(table, cols, vals, cb) {
+        var dbQuery = "INSERT INTO" + table + " (" + cols.toString() + ") " + "VALUES (" + createQmarks(vals.length) + ") ";
+
+        console.log(dbQuery);
+
+        connection.query(dbQuery, function(err, res){
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    }
+    updateOne: function(table, objColVals, condition, cb) {
+        var dbQuery = "UPDATE " + table + " SET" + translateSql(objColVals) + " WHERE " + condition;
+
+        console.log(dbQuery);
+
+        connection.query(dbQuery, function(err, res){
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    }
+
+};
+
+  
+module.exports = orm;
